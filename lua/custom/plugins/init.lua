7621257cb4c6a623a84e369c5242b784b9057a24
codelotusr <smaliukasdeividas@gmail.com>
2024-11-02 00:37:28 +0300

    Add more plugins | Configure plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -459,4 +459,46 @@ return {
   {
     { 'akinsho/bufferline.nvim', version = '*', dependencies = 'nvim-tree/nvim-web-devicons' },
   },
+  {
+    'tweekmonster/django-plus.vim',
+  },
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'thenbe/neotest-playwright',
+      'nvim-neotest/neotest-python',
+      'haydenmeade/neotest-jest',
+    },
+    config = function()
+      require('neotest').setup {
+        adapters = {
+          require 'neotest-python' {
+            dap = { justMyCode = false },
+
+            args = { '--log-level', 'DEBUG' },
+
+            runner = 'pytest',
+          },
+          require 'neotest-jest' {
+            jestCommand = 'npm test --',
+            jestConfigFile = 'custom.jest.config.ts',
+            env = { CI = true },
+            cwd = function(path)
+              return vim.fn.getcwd()
+            end,
+          },
+          require('neotest-playwright').adapter {
+            options = {
+              persist_project_selection = true,
+              enable_dynamic_test_discovery = true,
+            },
+          },
+        },
+      }
+    end,
+  },
 }
